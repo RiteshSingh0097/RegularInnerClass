6ab6149e664cb61c71da1a93e4831dc7706ec19b
Ritesh Singh <ritesh893@outlook.com>
2018-08-15 19:23:03 +0530

    new java class

--- src/DecimalConversion.java
+++ src/DecimalConversion.java
@@ -5,9 +5,9 @@ public class DecimalConversion {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
 
-        System.out.println("Binary : "+Integer.parseInt(str,2));
-        System.out.println("Octal : "+Integer.parseInt(str,8));
-        System.out.println("Decimal : "+Integer.parseInt(str,10));
+        //System.out.println("Binary : "+Integer.parseInt(str,2));
+        //System.out.println("Octal : "+Integer.parseInt(str,8));
+        //System.out.println("Decimal : "+Integer.parseInt(str,10));
         System.out.println("Hexa : "+Integer.parseInt(str,16));
     }
 }
